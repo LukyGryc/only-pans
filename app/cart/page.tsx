7ac2e56c86a89aabecf07c8e5c7f33f06ccf7c43
LukyGryc <lukygryc@seznam.cz>
2026-01-24 21:52:12 +0100

    summary

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -9,6 +9,7 @@ import { ProductWithQuantity } from "@/types/products";
 import { Home } from "lucide-react";
 import { getPriceFormatted } from "@/util/productUtil";
 import SummaryItem from "./SummaryItem";
+import SummaryOrder from "./SummaryOrder";
 
 const Cart = () => {
 
@@ -48,24 +49,7 @@ const Cart = () => {
                 ))}
                 </div>
 
-                {/* Summary */}
-                <div className="w-full lg:max-w-sm bg-gray-50 rounded-xl p-6 h-fit">
-                    <h2 className="text-lg font-semibold mb-4">Order Summary</h2>
-
-                    <div className="flex justify-between text-sm mb-2">
-                        <span className="text-gray-600">Items</span>
-                        <span>{ items.reduce((acc, { quantity }) => acc + quantity, 0) }</span>
-                    </div>
-
-                    <div className="flex justify-between font-semibold text-lg border-t pt-4">
-                        <span>Total</span>
-                        <span>{ getPriceFormatted(items.reduce((acc, { price, quantity }) => acc + price * quantity , 0)) }</span>
-                    </div>
-
-                    <button className="mt-6 w-full bg-gray-900 text-white py-3 rounded-xl hover:bg-gray-800 transition">
-                        Checkout
-                    </button>
-                </div>
+                <SummaryOrder items={items} />
             </Content>
         </Page>
   )
